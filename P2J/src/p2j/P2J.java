15e2fb78d11710e08da1bf297fd75f3be4151e3c
Dima <Dima@Dima-win7>
2013-06-16 23:16:37 +0400

    ok

--- P2J/src/p2j/P2J.java
+++ P2J/src/p2j/P2J.java
@@ -1,63 +1,122 @@
 package p2j;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
 import org.antlr.v4.runtime.ANTLRFileStream;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 
 public class P2J {
         static ArrayList<Statement> program;
-        static Statement curStatement;
-        static Structure query;
-        static HashMap<String, Variable> queryVars;
+//        static Statement curStatement;
+        public static Structure query;
+        public static HashMap<String, Variable> queryVars;
+        public static StringBuffer textSB = new StringBuffer();
         
     public static void main(String[] args) throws IOException {
-        PrologLexer pl=new PrologLexer((CharStream) new ANTLRFileStream(args[0])); 
-        PrologParser pp=new PrologParser(new CommonTokenStream(pl));
-        Listener listener=new Listener();
-        pp.addParseListener(listener);
-        pp.program();
-        
-        for (Statement st:program) {
-            System.out.println(st.toString());
+        if (args==null||args.length==0) {  //GUI
+            P2JFrame frame = new P2JFrame();
+            frame.setVisible(true);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         }
-        System.out.println();
-        
-        pl=new PrologLexer((CharStream) new ANTLRFileStream(args[1])); 
-        pp=new PrologParser(new CommonTokenStream(pl));
-        QueryListener qListener=new QueryListener();
-        pp.addParseListener(qListener);
-        pp.program();
-        System.out.println(query+"\n");
-        queryVars = new HashMap<String, Variable>();
-        addAndRenameVarsFrom(query);
-        //output vars
-        Unificator unificator = new Unificator(query);
-        Unificator.setProgram(program);
-        boolean result = unificator.unifyProgram();
-        System.out.println(unificator.getMap());
-        
-        outPutToString(unificator.getMap(), result);
+        if (args.length==1) { //compile
+            String sourceName=args[0];
+            File source=new File(sourceName);
+            if (!source.exists()) {
+                System.out.println("File \""+sourceName+"\" doesn't exist");
+                return;
+            }
+            if (!source.canRead()) {
+                System.out.println("File \""+sourceName+"\" cannot be read");
+                return;
+            }
+            try {
+                
+                Compiler.compile(args[0]);
+            } catch (Exception e) {
+                System.out.println("Something got wrong");
+                e.printStackTrace();
+            } finally {
+                return;
+            }             
+        }
+        if (args.length>1) { //interpret
+            String programName=args[0];
+            String queryName=args[1];
+            File programFile=new File(programName);
+            File queryFile=new File(queryName);
+            if (!programFile.exists()) {
+                System.out.println("File \""+programName+"\" doesn't exist");
+                return;
+            }
+            if (!programFile.canRead()) {
+                System.out.println("File \""+programName+"\" cannot be read");
+                return;
+            }
+             if (!queryFile.exists()) {
+                System.out.println("File \""+queryName+"\" doesn't exist");
+                return;
+            }
+            if (!queryFile.canRead()) {
+                System.out.println("File \""+queryName+"\" cannot be read");
+                return;
+            }
+            try {
+            //interpreter
+            PrologLexer pl=new PrologLexer((CharStream) new ANTLRFileStream(args[0])); 
+            PrologParser pp=new PrologParser(new CommonTokenStream(pl));
+            Listener listener=new Listener();
+            pp.addParseListener(listener);
+            pp.program();
+
+            pl=new PrologLexer((CharStream) new ANTLRFileStream(args[1])); 
+            pp=new PrologParser(new CommonTokenStream(pl));
+            QueryListener qListener=new QueryListener();
+            pp.addParseListener(qListener);
+            pp.program();
+            System.out.println(query);
+            textSB.append(query).append("\n").append("\n");
+            
+            queryVars = new HashMap<String, Variable>();
+            addAndRenameVarsFrom(query);
+            //output vars
+            Unificator unificator = new Unificator(query);
+            Unificator.setProgram(program);
+            boolean result = unificator.unifyProgram();
+//            System.out.println(unificator.getMap());
+            outPutToString(unificator.getMap(), result);
+            } catch (Exception e) {
+                System.out.println("Something got wrong");
+                e.printStackTrace();
+            } finally {
+                //return;
+            }      
+        }
     }
 
-    private static void addAndRenameVarsFrom(Structure structure) {
+    public static void addAndRenameVarsFrom(Structure structure) {
     
         for (int i=0; i< structure.args.size(); i++) {
             Argument arg = structure.args.get(i);
-            if (arg.getClass().getSimpleName().equals("Variable")) {
-                Variable var = new Variable(((Variable) arg).name+"#");
-                structure.args.set(i, var);
-                if (!queryVars.containsKey(var.name)) {
-                    queryVars.put(var.name ,var);
-                }
-                
-            } else if (arg.getClass().getSimpleName().equals("List")){
-                addAndRenameVarsFrom((List) arg);
-            } else if (arg.getClass().getSimpleName().equals("Structure")) {
-                addAndRenameVarsFrom((Structure) arg);
-            } 
+            switch (arg.getClass().getSimpleName()) {
+                case "Variable":
+                    Variable var = new Variable(((Variable) arg).name+"#");
+                    structure.args.set(i, var);
+                    if (!queryVars.containsKey(var.name)) {
+                        queryVars.put(var.name ,var);
+                    }
+                    break;
+                case "List":
+                    addAndRenameVarsFrom((List) arg);
+                    break;
+                case "Structure": 
+                    addAndRenameVarsFrom((Structure) arg);
+                    break;
+            }
         }
     }
     
@@ -66,43 +125,47 @@ public class P2J {
         
         for (int i=0; i< list.args.size(); i++) {
             Argument arg = list.args.get(i);
-            if (arg.getClass().getSimpleName().equals("Variable")) {
-                Variable var = new Variable(((Variable) arg).name+"#");
-                list.args.set(i, var);
-                if (!queryVars.containsKey(var.name)) {
-                    queryVars.put(var.name,var);
-                }
-                
-            } else if (arg.getClass().getSimpleName().equals("List")){
-                addAndRenameVarsFrom((List) arg);
-            } else if (arg.getClass().getSimpleName().equals("Structure")) {
-                addAndRenameVarsFrom((Structure) arg);
-            } 
+            switch (arg.getClass().getSimpleName()) {
+                case "Variable":
+                    Variable var = new Variable(((Variable) arg).name+"#");
+                    list.args.set(i, var);
+                    if (!queryVars.containsKey(var.name)) {
+                        queryVars.put(var.name,var);
+                    }
+                    break;
+                case "List":
+                    addAndRenameVarsFrom((List) arg);
+                    break;
+                case "Structure": 
+                    addAndRenameVarsFrom((Structure) arg);
+                    break;
+            }
         }
     }
     
     public static void outPutToString(HashMap<String, Argument> map, boolean result) {
         
         
-        System.out.println();
         if (result) {
             if (!queryVars.isEmpty()) {
-                System.out.println(query);
+
                 map = backRenameInMap(pullUpVariables(map));
-                System.out.println(map);
-                System.out.println(queryVars);
+//                System.out.println(map);
+//                System.out.println(queryVars);
                 for (String s : queryVars.keySet()) {
                     Variable var = queryVars.get(s);
                     if (map.containsKey(var.name)) {
-
+                        textSB.append("Variable ").append(var.name.substring(0, var.name.length()-1)).append(" = ").append(map.get(var.name)).append("\n");
                         System.out.println("Variable "+var.name.substring(0, var.name.length()-1)+" = "+map.get(var.name));
                     } else {
+                        textSB.append("Variable ").append(var.name.substring(0, var.name.length()-1)).append(" is free"+"\n");
                         System.out.println("Variable "+var.name.substring(0, var.name.length()-1)+" is free");
                     }
 
                 }
             }     
         }
+        textSB.append(result);
         System.out.println(result);
     }
     
