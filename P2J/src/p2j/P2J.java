a7957ea5efa8ac6767eba209812d37ca58755800
Dima <Dima@Dima-win7>
2013-06-16 18:30:15 +0400

    ok

--- P2J/src/p2j/P2J.java
+++ P2J/src/p2j/P2J.java
@@ -3,7 +3,6 @@ package p2j;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
 import org.antlr.v4.runtime.ANTLRFileStream;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CommonTokenStream;
@@ -53,6 +52,7 @@ public class P2J {
                 if (!queryVars.containsKey(var.name)) {
                     queryVars.put(var.name ,var);
                 }
+                
             } else if (arg.getClass().getSimpleName().equals("List")){
                 addAndRenameVarsFrom((List) arg);
             } else if (arg.getClass().getSimpleName().equals("Structure")) {
@@ -63,7 +63,6 @@ public class P2J {
     
     
     private static void addAndRenameVarsFrom(List list) {
-        
         
         for (int i=0; i< list.args.size(); i++) {
             Argument arg = list.args.get(i);
@@ -84,23 +83,88 @@ public class P2J {
     
     public static void outPutToString(HashMap<String, Argument> map, boolean result) {
         
+        
         System.out.println();
         if (result) {
-            
-            System.out.println(query);
-            for (String s : queryVars.keySet()) {
-                Variable var = queryVars.get(s);
-                System.out.println("Variable "+var.name.substring(0, var.name.length()-1)+" = "+map.get(var.name));
-            }
-            for (Argument arg : query.args) {
-                if (arg.getClass().getSimpleName().equals("Variable")) {
-                    Variable var = (Variable) arg;
-                    if (!queryVars.containsKey(var.name)) {
-                        System.out.println("Variable "+var.name+" is free");
+            if (!queryVars.isEmpty()) {
+                System.out.println(query);
+                map = backRenameInMap(pullUpVariables(map));
+                System.out.println(map);
+                System.out.println(queryVars);
+                for (String s : queryVars.keySet()) {
+                    Variable var = queryVars.get(s);
+                    if (map.containsKey(var.name)) {
+
+                        System.out.println("Variable "+var.name.substring(0, var.name.length()-1)+" = "+map.get(var.name));
+                    } else {
+                        System.out.println("Variable "+var.name.substring(0, var.name.length()-1)+" is free");
+                    }
+
+                }
+            }     
+        }
+        System.out.println(result);
+    }
+    
+    public static HashMap<String, Argument> backRenameInMap(HashMap<String, Argument> map) {
+        
+        HashMap<String, Argument> tempMap = (HashMap<String, Argument>) map.clone();
+        for (String s : map.keySet()) {
+            Argument arg = map.get(s);
+            tempMap.put(s, backRenameArgument(arg));
+        }
+        return tempMap;
+    }
+    
+    public static Argument backRenameArgument(Argument arg) {
+        
+        switch (arg.getClass().getSimpleName()) {
+            case "Variable" :
+                Variable var = (Variable) arg;
+                if (var.name.contains("#")) {
+                    var = new Variable(var.name.substring(0, var.name.length()-1));
+                    //var.name = var.name.substring(0, var.name.length()-1);
+                }
+                return var;
+            case "Structure" :
+                return backRenameInStructure((Structure) arg);
+            case "List" :
+                return backRenameInList((List) arg);
+            default :
+                return arg;
+        }
+    }
+    
+    public static List backRenameInList(List list) {
+        
+        for (int i = 0; i < list.args.size(); i++) {
+            list.args.set(i, backRenameArgument(list.args.get(i)));
+        }
+        return list;
+    }
+    
+    public static Structure backRenameInStructure(Structure structure) {
+        
+        for (int i = 0; i < structure.args.size(); i++) {
+            structure.args.set(i, backRenameArgument(structure.args.get(i)));
+        }
+        return structure;
+    }
+    
+    public static HashMap<String, Argument> pullUpVariables(HashMap<String, Argument> map) {
+        
+        HashMap<String, Argument> tempMap = (HashMap<String, Argument>) map.clone();
+        for (String s : map.keySet()) {
+            if (queryVars.containsKey(s)) {
+                Variable var = (Variable) queryVars.get(s);
+                if (map.get(var.name).getClass().getSimpleName().equals("Variable")) {
+                    Variable newVar = (Variable) map.get(var.name);
+                    if ((!queryVars.containsKey(newVar.name))&&(map.containsKey(newVar.name))) {
+                        tempMap.put(s, map.get(newVar.name));
                     }
                 }
             }
         }
-        System.out.println(result);
+        return tempMap;
     }
 }
